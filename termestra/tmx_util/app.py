f8d8a0c1b4c23a1e0e63416b038e425150ad1bc6
Mark Deric <codesmith@dericnet.com>
2024-05-22 22:11:49 -0700

    Add line buffering and "app" object API idea
    Signed-off-by: Mark Deric <codesmith@dericnet.com>

--- termestra/tmx_util/app.py
+++ termestra/tmx_util/app.py
@@ -5,6 +5,7 @@ import logging
 import os
 from functools import partial
 from signal import SIGINT, SIGTERM, Signals
+from time import time
 
 from . import tmux
 from .misc import run_cmd
@@ -21,6 +22,9 @@ class AppBase:
         self.sigs = (SIGINT, SIGTERM)
         self.halt = False  # stop requested
         self.done = False  # stop procedures complete
+
+        # data_received line buffering support
+        self.b_siz = 4096
 
         for sess_name in tab_name_list:
             self.tmux_mgr.add_session(sess_name)
@@ -43,6 +47,11 @@ class AppBase:
             # add disconnected status and pipe's transport to the TmuxSession object
             tms.disconnected = None
             tms.transport = None
+            # add data_received line buffering support to the TmuxSession object
+            tms.line_start_time = None
+            # revisit memoryview buffering
+            tms.line_buffer = memoryview(bytearray(self.b_siz))
+            tms.b_start = 0
 
     async def _connect_pipe(self, sess_name, pipe):
         tp = await self.loop.connect_read_pipe(
@@ -53,21 +62,74 @@ class AppBase:
     def connection_made(self, sess_name, transport):
         tms = self.tmux_mgr.tmux_session_map[sess_name]
         tms.disconnected = False
-        logger.info(f"Connected: '{sess_name}' with transport {transport!r}")
+        logger.info(f"connection_made: '{sess_name}' with transport {transport!r}")
 
     def connection_lost(self, sess_name, exc):
         tms = self.tmux_mgr.tmux_session_map[sess_name]
         tms.disconnected = True
-        logger.info(f"Disconnected: '{sess_name}'")
+        logger.info(f"connection_lost: '{sess_name}'")
         if exc is not None:
             raise exc
 
     def data_received(self, sess_name, data):
-        logger.debug(f"DataReceived: '{sess_name}' data: {data}")
+        d_siz = len(data)
+        tms = self.tmux_mgr.tmux_session_map[sess_name]
+        logger.debug(f"data_received '{sess_name}'")
+        now = time()
+        if tms.line_start_time is None:
+            tms.line_start_time = now
+
+        get_start = 0
+        b_room = None
+        while True:
+            # choke on the number of copies we make FIXME
+            if get_start:
+                d_siz -= b_room
+            b_room = self.b_siz - tms.b_start
+            end = tms.b_start + (b_room if d_siz > b_room else d_siz)
+            logger.debug(
+                f"recv_loop get_start: {get_start}; d_siz: {d_siz}; "
+                f"b_start: {tms.b_start}; b_room: {b_room}; end: {end}"
+            )
+            if d_siz > b_room:
+                tms.line_buffer[tms.b_start :] = data[get_start : get_start + b_room]
+                get_start += b_room
+                data_fits = False
+            else:
+                tms.line_buffer[tms.b_start : end] = data[get_start:]
+                data_fits = True
+
+            snippet = bytes(tms.line_buffer[:end])
+            # lines is a copy out
+            lines = snippet.split(b"\r\n")
+            line_count = len(lines)
+            if line_count > 1:
+                # there are some complete lines
+                residual = lines[line_count - 1]
+                residual_siz = len(residual)
+                if residual_siz > 0:
+                    tms.line_buffer[:residual_siz] = residual
+                    lines[line_count - 1] = b""
+                    tms.b_start = residual_siz
+                else:
+                    tms.b_start = 0
+                self.data_to_app(sess_name, lines)
+                if data_fits:
+                    break
+            else:  # only one line, no \r\n
+                if data_fits:
+                    tms.b_start += d_siz
+                    break
+                else:
+                    tms.b_start = 0
+                    self.data_to_app(sess_name, lines)
+
+    def data_to_app(self, sess_name, lines):
+        logger.debug(f"data_to_app '{sess_name}' -- lines: {lines}")
 
     def housekeeping(self):
-        logger.info(f"Housekeeping at {self.next_time}")
         if self.halt:
+            logger.debug("housekeeping called to halt")
             for sess_name in self.tmux_mgr.tmux_session_map:
                 tms = self.tmux_mgr.tmux_session_map[sess_name]
                 if not tms.transport.is_closing():
